dbfbf4838b262528b41ddf8c3ff1c045a9a9845e
Martin Stannard <martin@stannard.id.au>
2020-10-28 20:44:56 +1100

    add rust gigasecond

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,16 +1,9 @@
 class ResistorColorDuo
-  BANDS = %w[black brown red orange yellow green blue violet grey white]
+  BAND = %w[black brown red orange yellow green blue violet grey white]
 
   def self.value(colors)
-    colors.take(2).reduce('') do |str, c|
-      str + index(c).to_s
+    colors.take(2).reduce('') do |digits, color|
+      digits << BAND.index(color).to_s
     end.to_i
   end
-
-  private
-
-  def self.index(color)
-    BANDS.index(color)
-  end
-
 end
