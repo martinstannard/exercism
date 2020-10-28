e5103b69173946c39b15d3104d1c025364bd47d5
Martin Stannard <martin@stannard.id.au>
2020-10-28 21:42:53 +1100

    julia pangram

--- julia/pangram/pangram.jl
+++ julia/pangram/pangram.jl
@@ -5,6 +5,12 @@ Return `true` if `input` contains every alphabetic character (case insensitive).
 
 """
 function ispangram(input)
-
+    counts = zeros(26)
+    for (index, value) in enumerate(lowercase(input))
+        i = 'z' - value
+        if i >= 0 && i < 26 && counts[i+1] == 0.0
+            counts[i+1] = 1
+        end
+    end
+    sum(counts) == 26
 end
-
