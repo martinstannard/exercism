ebbe38f419b809d49890b398769c2355cdb2255e
Martin Stannard <martin@stannard.id.au>
2024-06-22 13:59:00 +1000

    more changes

--- elixir/bowling/lib/bowling.ex
+++ elixir/bowling/lib/bowling.ex
@@ -1,109 +1,117 @@
 defmodule Bowling do
-
-  @current_score 0
-
   @doc """
     Creates a new game of bowling that can be used to store the results of
     the game
   """
 
+  @current_score 0
+
   @spec start() :: any
   def start do
     %{
       current_score: 0,
+      extra_score: 0,
       frame: 1,
-      roll_number: 0,
-      previous_roll: 0,
-      was_spare: false,
-      strike_count: 0,
-      double_count: 0
+      pins_left: 10,
+      roll: 1,
     }
   end
 
   @doc """
     Records the number of pins knocked down on a single roll. Returns `any`
     unless there is something wrong with the given number of pins, in which
-    case it returns a helpful message.
+    case it returns a helpful error tuple.
   """
 
-  @spec roll(any, integer) :: any | String.t()
+  @spec roll(any, integer) :: {:ok, any} | {:error, String.t()}
   def roll(game, roll) do
-    handle_roll(game, roll)
+    {:ok, handle_roll(game, roll)}
   end
 
   @doc """
     Returns the score of a given game of bowling if the game is complete.
-    If the game isn't complete, it returns a helpful message.
+    If the game isn't complete, it returns a helpful error tuple.
   """
 
-  @spec score(any) :: integer | String.t()
+  @spec score(any) :: {:ok, integer} | {:error, String.t()}
   def score(game) do
-    game[:current_score]
+    {:ok, game[:current_score]}
   end
 
   # strike
-  defp handle_roll(%{roll_number: 0} = game, 10) do
-    IO.puts "strike"
+  defp handle_roll(%{roll: 1} = game, 10) do
+    IO.inspect 10
     IO.inspect game
-    %{ game |
-       previous_roll: 10,
-       current_score: roll_score(game, 10),
-       double_count: game.double_count + 2,
-       roll_number: 0,
-       previous_roll: 10,
-       frame: game.frame + 1
+    %{game |
+      current_score: roll_score(game, 10),
+      extra_score: game.extra_score + 2,
+      frame: game.frame + 1,
+      pins_left: 10,
+      roll: 1
     }
   end
 
-  # 2nd ball
-  defp handle_roll(%{roll_number: 1} = game, roll) do
-    IO.puts "2nd ball"
-    IO.inspect game
-    IO.inspect roll
-    was_spare = roll + game.previous_roll == 10
-    spare = spare(game.previous_roll, roll)
-    %{ game |
-       previous_roll: roll,
-       current_score: roll_score(game, roll),
-       double_count: spare,
-       roll_number: 0,
-       frame: game.frame + 1
+  # spare frame 10
+  defp handle_roll(%{frame: 10, roll: 2, pins_left: pins} = game, pins) do
+    %{game |
+      current_score: roll_score(game, pins),
+      extra_score: 0,
+      frame: game.frame + 1,
+      pins_left: 10,
+      roll: 1
     }
   end
 
-  # 1st ball
-  defp handle_roll(%{roll_number: 0} = game, roll) do
-    IO.puts "1st ball"
+  # spare
+  defp handle_roll(%{roll: 2, pins_left: pins} = game, pins) do
+    IO.inspect pins
     IO.inspect game
-    IO.inspect roll
-    %{ game |
-       previous_roll: roll,
-       current_score: roll_score(game, roll),
-       double_count: counter(game),
-       roll_number: 1,
+    %{game |
+      current_score: roll_score(game, pins),
+      extra_score: 1,
+      frame: game.frame + 1,
+      pins_left: 10,
+      roll: 1
     }
   end
 
-  defp roll_score(%{frame: 11} = game, roll) do
-    game.current_score + roll * 2
+  # plain hit
+  defp handle_roll(%{roll: 1} = game, pins) do
+    IO.inspect pins
+    IO.inspect game
+    %{game |
+      current_score: roll_score(game, pins),
+      extra_score: extra_score(game),
+      pins_left: 10 - pins,
+      roll: 2
+    }
   end
 
-  defp roll_score(%{double_count: dc} = game, roll) when dc > 0 do
-    game.current_score + roll * 2
+  #
+  defp handle_roll(%{roll: 2} = game, pins) do
+    IO.inspect pins
+    IO.inspect game
+    %{game |
+      current_score: roll_score(game, pins),
+      extra_score: extra_score(game),
+      frame: game.frame + 1,
+      pins_left: 10,
+      roll: 1
+    }
+  end
+
+  # defp roll_score(%{frame: 11} = game, roll) do
+  #   game.current_score + roll * 2
+  # end
+
+  defp roll_score(%{extra_score: extra} = game, roll) when extra > 0 do
+    game.current_score + roll * extra
   end
 
   defp roll_score(game, roll) do
-    game.current_score + roll
+     game.current_score + roll
   end
 
-  defp counter(%{double_count: dc}) when dc > 0 do
-    dc - 1
-  end
-
-  defp counter(_) do
-    0
-  end
-
-  defp spare(previous, roll) when (previous + roll == 10), do: 1
-  defp spare(_, _), do: 0
+  defp extra_score(%{extra_score: extra}) when extra > 0, do: extra - 1
+  defp extra_score(_), do: 0
 end
--- elixir/bowling/test/bowling_test.exs
+++ elixir/bowling/test/bowling_test.exs
@@ -2,79 +2,73 @@ defmodule BowlingTest do
   use ExUnit.Case
 
   defp roll_reduce(game, rolls) do
-    Enum.reduce(rolls, game, fn roll, game -> Bowling.roll(game, roll) end)
+    Enum.reduce(rolls, game, fn roll, game ->
+      {:ok, game} = Bowling.roll(game, roll)
+      game
+    end)
   end
 
-  @tag :pending
   test "should be able to score a game with all zeros" do
     game = Bowling.start()
     rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 0
+    assert Bowling.score(game) == {:ok, 0}
   end
 
-  @tag :pending
   test "should be able to score a game with no strikes or spares" do
     game = Bowling.start()
     rolls = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 90
+    assert Bowling.score(game) == {:ok, 90}
   end
 
-  @tag :pending
   test "a spare followed by zeros is worth ten points" do
     game = Bowling.start()
     rolls = [6, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 10
+    assert Bowling.score(game) == {:ok, 10}
   end
 
-  @tag :pending
   test "points scored in the roll after a spare are counted twice" do
     game = Bowling.start()
     rolls = [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 16
+    assert Bowling.score(game) == {:ok, 16}
   end
 
-  @tag :pending
   test "consecutive spares each get a one roll bonus" do
     game = Bowling.start()
     rolls = [5, 5, 3, 7, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 31
+    assert Bowling.score(game) == {:ok, 31}
   end
 
-  @tag :pending
   test "a spare in the last frame gets a one roll bonus that is counted once" do
     game = Bowling.start()
     rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 7]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 17
+    assert Bowling.score(game) == {:ok, 17}
   end
 
-  @tag :pending
   test "a strike earns ten points in a frame with a single roll" do
     game = Bowling.start()
     rolls = [10, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 10
+    assert Bowling.score(game) == {:ok, 10}
   end
 
-  @tag :pending
   test "points scored in the two rolls after a strike are counted twice as a bonus" do
     game = Bowling.start()
     rolls = [10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 26
+    assert Bowling.score(game) == {:ok, 26}
   end
 
-  # @tag :pending
   test "consecutive strikes each get the two roll bonus" do
     game = Bowling.start()
     rolls = [10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 81
+    assert Bowling.score(game) == {:ok, 81}
   end
 
   @tag :pending
@@ -82,7 +76,7 @@ defmodule BowlingTest do
     game = Bowling.start()
     rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 1]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 18
+    assert Bowling.score(game) == {:ok, 18}
   end
 
   @tag :pending
@@ -90,7 +84,7 @@ defmodule BowlingTest do
     game = Bowling.start()
     rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 3]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 20
+    assert Bowling.score(game) == {:ok, 20}
   end
 
   @tag :pending
@@ -98,7 +92,15 @@ defmodule BowlingTest do
     game = Bowling.start()
     rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 30
+    assert Bowling.score(game) == {:ok, 30}
+  end
+
+  @tag :pending
+  test "last two strikes followed by only last bonus with non strike points" do
+    game = Bowling.start()
+    rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 0, 1]
+    game = roll_reduce(game, rolls)
+    assert Bowling.score(game) == {:ok, 31}
   end
 
   @tag :pending
@@ -106,7 +108,7 @@ defmodule BowlingTest do
     game = Bowling.start()
     rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 20
+    assert Bowling.score(game) == {:ok, 20}
   end
 
   @tag :pending
@@ -114,7 +116,7 @@ defmodule BowlingTest do
     game = Bowling.start()
     rolls = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 300
+    assert Bowling.score(game) == {:ok, 300}
   end
 
   @tag :pending
@@ -132,7 +134,7 @@ defmodule BowlingTest do
   @tag :pending
   test "two rolls in a frame cannot score more than 10 points" do
     game = Bowling.start()
-    game = Bowling.roll(game, 5)
+    {:ok, game} = Bowling.roll(game, 5)
     assert Bowling.roll(game, 6) == {:error, "Pin count exceeds pins on the lane"}
   end
 
@@ -157,7 +159,7 @@ defmodule BowlingTest do
     game = Bowling.start()
     rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 6]
     game = roll_reduce(game, rolls)
-    assert Bowling.score(game) == 26
+    assert Bowling.score(game) == {:ok, 26}
   end
 
   @tag :pending
