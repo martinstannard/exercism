4841a5c0db44ea0f940e293d53e1bf0fc6f92ef6
Martin Stannard <martin@stannard.id.au>
2024-06-24 13:44:45 +1000

    better insertion

--- elixir/binary-search-tree/lib/binary_search_tree.ex
+++ elixir/binary-search-tree/lib/binary_search_tree.ex
@@ -13,26 +13,15 @@ defmodule BinarySearchTree do
   Creates and inserts a node with its value as 'data' into the tree.
   """
   @spec insert(bst_node, any) :: bst_node
+  def insert(nil, data) do
+    %{data: data, left: nil, right: nil}
+  end
   def insert(tree, data) do
     if tree.data >= data do
-      %{data: tree.data, left: insert_left(tree.left, data), right: tree.right}
+      %{data: tree.data, left: insert(tree.left, data), right: tree.right}
     else
-      %{data: tree.data, left: tree.left, right: insert_right(tree.right, data)}
+      %{data: tree.data, left: tree.left, right: insert(tree.right, data)}
     end
-  end
-
-  def insert_left(nil, data) do
-    %{data: data, left: nil, right: nil}
-  end
-  def insert_left(tree, data) do
-    insert(tree, data)
-  end
-
-  def insert_right(nil, data) do
-    %{data: data, left: nil, right: nil}
-  end
-  def insert_right(tree, data) do
-    insert(tree, data)
   end
 
   @doc """
