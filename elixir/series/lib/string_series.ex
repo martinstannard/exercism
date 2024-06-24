9b16e051307fdc70383aa7195bf8989b38340fe9
Martin Stannard <martin@stannard.id.au>
2024-06-24 12:52:55 +1000

    better series

--- elixir/series/lib/string_series.ex
+++ elixir/series/lib/string_series.ex
@@ -9,25 +9,7 @@ defmodule StringSeries do
 
   def slices(s, size) do
     s
-    |> to_list
-    |> extract(size, [])
-    |> Enum.reverse()
-  end
-
-  defp extract(digits, length, accum) do
-    digits
-    |> Enum.slice(0..(length - 1))
-    |> do_extract(digits, length, accum)
-  end
-
-  defp do_extract(substring, _digits, length, accum) when length(substring) < length, do: accum
-  defp do_extract(substring, [_h | rest], length, accum) do
-    extract(rest, length, [Enum.join(substring, "") | accum])
-  end
-
-  defp to_list(string) do
-    string
-    |> String.split("")
-    |> Enum.reject(fn s -> s == "" end)
-  end
+    |> String.split("", trim: true)
+    |> Enum.chunk_every(size, 1, :discard)
+    |> Enum.map(&Enum.join/1)
 end
