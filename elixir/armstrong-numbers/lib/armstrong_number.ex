eaeaca1d06856b086f8c74abb167687faa1dc300
Martin Stannard <martin@stannard.id.au>
2024-06-24 12:59:04 +1000

    better armstrong

--- elixir/armstrong-numbers/lib/armstrong_number.ex
+++ elixir/armstrong-numbers/lib/armstrong_number.ex
@@ -9,31 +9,25 @@ defmodule ArmstrongNumber do
     sum =
       number
       |> digits
-      |> sum_digits(number_length(number))
+      |> sum_digits
 
     sum == number
   end
 
-  @spec sum_digits([integer], integer) :: integer
-  defp sum_digits(digits, power) do
+  @spec sum_digits([integer]) :: integer
+  defp sum_digits(digits) do
+    power = length(digits)
     digits
     |> Enum.reduce(0, fn digit, accum ->
       accum + Integer.pow(digit, power)
     end)
   end
 
-  defp number_length(number) do
-    number
-    |> Integer.to_string()
-    |> String.length()
-  end
-
   @spec digits(integer) :: [integer]
   defp digits(number) do
     number
     |> Integer.to_string()
-    |> String.split("")
-    |> Enum.reject(fn s  -> s == "" end)
-    |> Enum.map(&(String.to_integer/1))
+    |> String.split("", trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
 end
